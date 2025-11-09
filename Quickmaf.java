31472a9f763342ce0bafb6aaddc88eb58f6775b2
HaroonAlQasmi <haroonalqasmi.pro@gmail.com>
2025-11-09 08:27:58 +0400

    leaning Variables

---
+++ Quickmaf.java
@@ -0,0 +1,8 @@
+public class Quickmaf{
+    public static void main(String[] args){
+        System.out.println("Hallo Students");
+        System.out.print("1 + 1 = ");
+        System.out.println(1+1);
+        System.out.println("Thats Quickmaf");
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables{
+    public static void main(String[] args){
+        String firstName = "Haroon";
+        String lastName = " Al Qasmi";
+        String fullName = firstName + lastName;
+        int numberCent = 100;
+        System.out.println(fullName +" is "+ numberCent + " percent that Man" );
+    }
+}

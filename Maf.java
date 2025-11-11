062ac9b5db5523f6320100dc15c4ce4ee952a3ab
HaroonAlQasmi <haroonalqasmi.pro@gmail.com>
2025-11-11 08:06:25 +0400

    Quick maf

---
+++ Maf.java
@@ -0,0 +1,12 @@
+public class Maf{
+    public static void main(String[] args){
+        System.out.println("the Largest number is "+Math.max(3,9));
+        System.out.println("The Smallest number is "+Math.min(3,9));
+        System.out.println("the squair root of 4 is "+Math.sqrt(4));
+        System.out.println("The absolute of -4.2 is "+Math.abs(-4.2));
+        System.out.println("2 to the power of 4 is "+Math.pow(2,4));
+        System.out.println("Rounding 4.5 becomes "+Math.round(4.5));
+        System.out.println("Rounding 6.5 up to "+Math.ceil(6.5));
+        System.out.println("Rounding 6.5 down to "+Math.floor(6.5));
+    }
+}
